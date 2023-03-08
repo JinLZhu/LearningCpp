2903f23b6240922a6abc69790c3f046a51dd484f
JinLZhu <rains_lovelily_only@126.com>
2023-03-08 23:50:20 +0800

    New commit

---
+++ C/C_Primer_Plus_6th/chapter04/程序清单/4.8.floats.c
@@ -0,0 +1,18 @@
+// floats.c 一些浮点数整型修饰符的组合
+#include <stdio.h>
+
+int main(void)
+{
+    const double RENT = 3852.99;    //const 变量
+
+    printf("*%f\n", RENT);
+    printf("*%e\n", RENT);
+    printf("*%4.2f\n", RENT);
+    printf("*%3.1f\n", RENT);
+    printf("*10.3%f\n", RENT);
+    printf("*10.3E*\n", RENT);
+    printf("*%+4.2f\n", RENT);
+    printf("*%010.2f\n", RENT);
+
+    return 0;
+}
