d8e629d820fc7334738c01d917218d13b8accd87
JinLZhu <rains_lovelily_only@126.com>
2023-03-12 16:25:39 +0800

    New commit

--- C/C_Primer_Plus_6th/chapter04/程序清单/4.8.floats.c
+++ C/C_Primer_Plus_6th/chapter04/程序清单/4.8.floats.c
@@ -5,14 +5,14 @@ int main(void)
 {
     const double RENT = 3852.99;    //const 变量
 
-    printf("*%f\n", RENT);
-    printf("*%e\n", RENT);
-    printf("*%4.2f\n", RENT);
-    printf("*%3.1f\n", RENT);
-    printf("*10.3%f\n", RENT);
+    printf("*%f*\n", RENT);
+    printf("*%e*\n", RENT);
+    printf("*%4.2f*\n", RENT);
+    printf("*%3.1f*\n", RENT);
+    printf("*10.3%f*\n", RENT);
     printf("*10.3E*\n", RENT);
-    printf("*%+4.2f\n", RENT);
-    printf("*%010.2f\n", RENT);
+    printf("*%+4.2f*\n", RENT);
+    printf("*%010.2f*\n", RENT);
 
     return 0;
 }
