e03f9f036499ba96067219595404597b523aca71
JinLZhu <rains_lovelily_only@126.com>
2023-06-27 22:05:10 +0800

    new commit

--- CPP/黑马C++/.history/71 通讯录管理系统/Add_Contacts_20230520104840.h
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-using namespace std;
-
-void Add_Contacts(struct_contacts, arr_len);
--- CPP/黑马C++/.history/71 通讯录管理系统/Add_Contacts_20230522122336.h
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-using namespace std;
-
-void Add_Contacts(struct_contacts, int arr_len);
