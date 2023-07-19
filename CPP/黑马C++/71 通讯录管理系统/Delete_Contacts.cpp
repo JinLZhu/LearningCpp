b6acd88b91b446aea9ae25776b17acd243f8b0ce
JinLZhu <rains_lovelily_only@126.com>
2023-07-19 10:55:03 +0800

    New commit

--- CPP/黑马C++/71 通讯录管理系统/Delete_Contacts.cpp
+++ CPP/黑马C++/71 通讯录管理系统/Delete_Contacts.cpp
@@ -2,18 +2,18 @@
 
 void Delete_Contacts(struct contacts s[], std::string s_name, int arr_len, struct contacts s_e[]){
     std::cout << "s_name is " << s_name << std::endl;
-    std::cout << "arr_len is " << arr_len << std::endl;
     for(int i = 0; i < arr_len; ++i){
         if (s[i].string_name == s_name){
+            if (i == arr_len - 1){
+                s[i] = s_e[0];
+                break;
+            }
+
             for (int j=i; j < arr_len; ++j){
-                if (j == arr_len - 1){
-                    s[i] = s_e[0];
-                    break;
-                }
                 s[i] = s[i + 1];
             }
         }
         //std::cin.get();
-        std::cin.get();
+        //std::cin.get();
     }
 }
