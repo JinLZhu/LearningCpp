39e1562ad0d6a408808042f9174316cb3bf9dd14
JinLZhu <rains_lovelily_only@126.com>
2024-04-12 17:31:58 +0800

    Continue...

---
+++ runoob.com_CPP/5.c++注释/HelloWorld_注释.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //这是一个注释
+    cout << "Hello,World!";//放在语句后面
+    /* 这是注释 */
+    /*
+     * 注释也可以
+     * 跨行
+     */
+    return 0;
+}
