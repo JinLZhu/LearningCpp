da6af3aeb1eeb029615502a201affaae08690946
JinLZhu <rains_lovelily_only@126.com>
2024-04-16 09:42:43 +0800

    Continue...

---
+++ runoob.com_CPP/10.C++常量/StringConstant.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string greeting = "hello, runoob!";
+    cout << greeting;
+    cout << "\n";
+    string greeting2 = "hello,\
+ runoob!";
+    cout << greeting2;
+
+    return 0;
+}
