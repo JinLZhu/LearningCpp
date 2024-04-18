c5a55dd2b36aee5c0d0069e9880f8f69920a0ddb
JinLZhu <rains_lovelily_only@126.com>
2024-04-18 14:12:42 +0800

    Continue...

---
+++ runoob.com_CPP/17.C++数字/numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //数字定义和赋值
+    short s = 10;
+    int i = 1000;
+    long l = 1000000;
+    float f = 230.47;
+    double d = 30949.374;
+
+    cout << "short  s :" << s << endl;
+    cout << "int    i :" << i << endl;
+    cout << "long   l :" << l << endl;
+    cout << "float  f :" << f << endl;
+    cout << "double d :" << d << endl;
+
+    return 0;
+}
