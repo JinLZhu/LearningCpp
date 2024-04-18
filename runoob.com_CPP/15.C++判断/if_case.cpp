70d0dfed01908717711626fac92ba1dbccc42558
JinLZhu <rains_lovelily_only@126.com>
2024-04-18 09:41:50 +0800

    Continue..

---
+++ runoob.com_CPP/15.C++判断/if_case.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a0 = 0;
+    int a1 = 1;
+    bool b1 = false;
+    bool b2 = true;
+
+    if (a0)
+    {
+        cout << "This is a0." << endl;
+    }
+
+    if (a1)
+    {
+        cout << "This is a1." << endl;
+    }
+
+    if (b1)
+    {
+        cout << "This is b1." << endl;
+    }
+
+    if (b2)
+    {
+        cout << "This is b2." << endl;
+    }
+
+    return 0;
+}
