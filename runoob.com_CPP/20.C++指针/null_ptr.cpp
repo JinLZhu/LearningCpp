9fd6f60afeca349589a3a29f3be33da14a3aadc8
JinLZhu <rains_lovelily_only@126.com>
2024-04-22 15:53:44 +0800

    Continue...

---
+++ runoob.com_CPP/20.C++指针/null_ptr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *ptr = NULL;
+    cout << "ptr的值是：" << ptr << endl;
+    /*
+     * if(ptr) 如果ptr非空，则完成
+     * if(!ptr) 如果ptr为空，则完成
+     */
+
+    return 0;
+}
