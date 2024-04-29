65d2c30e3005a95ec0b812f2f55d25f8c2d96791
JinLZhu <rains_lovelily_only@126.com>
2024-04-29 15:21:36 +0800

    Continue...

---
+++ runoob.com_CPP/27.C++类与对象/class_destructor.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Line
+{
+    public:
+        void setLength( double Len );
+        double getLength( void );
+        Line();         // 这是构造函数声明
+        ~Line();        // 这是析构函数声明
+
+    private:
+        double length;
+};
+
+// 成员函数及构造函数定义
+Line::Line(void)
+{
+    cout << "Object is being created." << endl;
+}
+Line::~Line(void)
+{
+    cout << "Object is being deleted." << endl;
+}
+
+void Line::setLength( double len )
+{
+    length = len;
+}
+
+double Line::getLength( void )
+{
+    return length;
+}
+
+int main()
+{
+    Line line;
+    // 设置长度
+    line.setLength(6.0);
+    cout << "Length of line : " << line.getLength() << endl;
+
+    return 0;
+}
