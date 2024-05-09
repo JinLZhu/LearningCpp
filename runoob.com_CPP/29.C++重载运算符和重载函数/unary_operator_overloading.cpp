d6f02d0cb601625d2e88f15e4049e0bb19162c04
JinLZhu <rains_lovelily_only@126.com>
2024-05-09 13:15:27 +0800

    Continue...

--- runoob.com_CPP/29.C++重载运算符和重载函数/unary_operator_overloading.cpp
+++ runoob.com_CPP/29.C++重载运算符和重载函数/unary_operator_overloading.cpp
@@ -3,4 +3,48 @@ using namespace std;
 /*
  * 一元运算符只对一个操作数进行操作
  * 递增(++), 递减(--), 负号(-), 逻辑非(！)
+ * 一元运算符出现在操作对象的左边或者右边
+ * !obj, -obj, ++obj, obj++, obj--
  */
+
+class Distance
+{
+    private:
+        int feet;           // 0到无穷
+        int inches;         // 0到12
+
+    public:
+        // 构造函数
+        Distance(){
+            feet = 0;
+            inches = 0;
+        }
+        Distance( int f, int i){
+            feet = f;
+            inches = i;
+        }
+        // 显示距离的方法
+        void displayDistance()
+        {
+            cout << "F: " << feet << " I:" << inches << endl;
+        }
+        // 重载运算符（ - ）
+        Distance operator- ()
+        {
+            feet = -feet;
+            inches = -inches;
+            return Distance(feet, inches);
+        }
+};
+
+int main()
+{
+    Distance D1(11, 10), D2(-5, 11);
+    -D1;                        // 取相反数
+    D1.displayDistance();       // 距离D1
+
+    -D2;                        // 取相反数
+    D2.displayDistance();       // 距离D2;
+
+    return 0;
+}
