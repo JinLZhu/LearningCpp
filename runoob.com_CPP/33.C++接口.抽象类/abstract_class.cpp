65cfc5521b639634a10c26e4f6273b09c6c0bed3
JinLZhu <rains_lovelily_only@126.com>
2024-05-10 19:24:21 +0800

    Continue...

---
+++ runoob.com_CPP/33.C++接口.抽象类/abstract_class.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Shape
+{
+    public:
+        virtual int getArea() = 0;
+        void setWidth(int w)
+        {
+            width = w;
+        }
+        void setHeight(int h)
+        {
+            height = h;
+        }
+    protected:
+        int width;
+        int height;
+};
+
+// 派生类
+class Rectangle : public Shape
+{
+    public:
+        int getArea()
+        {
+            return (width * height);
+        }
+};
+class Triangle : public Shape
+{
+    public:
+        int getArea()
+        {
+            return (width * height)/2;
+        }
+};
+
+int main(void)
+{
+    Rectangle Rect;
+    Triangle Tri;
+
+    Rect.setWidth(5);
+    Rect.setHeight(7);
+    // 输出对象的面积
+    cout << "Total Triangle area: " << Rect.getArea() << endl;
+
+    Tri.setWidth(5);
+    Tri.setHeight(7);
+    // 输出对象的面积
+    cout << "Total Triangle area: " << Tri.getArea() << endl;
+
+    return 0;
+}
