5e4423ab7ad23606fe6458ab6b24e050b8f8b493
JinLZhu <rains_lovelily_only@126.com>
2024-05-10 20:16:28 +0800

    Continue...

--- runoob.com_CPP/34.C++文件和流/read_write_stream.cpp
+++ runoob.com_CPP/34.C++文件和流/read_write_stream.cpp
@@ -45,4 +45,27 @@ int main()
     infile.close();
 
     return 0;
+
+    /*
+     * istream 和 ostream都提供了用于重新定位文件位置指针的成员函数
+     * 这些成员函数包括关于istream和seekg（“seek get”）和关于ostream的seekp（“seek put”）
+     * seekg和seekp的参数通常是一个长整型
+     * 第二个参数可以用于指定查找方向
+     * 查找方向可以是ios::beg（默认的，从流的开头开始定位）
+     * 也可以是ios::cur（从流的当前位置开始定位）
+     * 也可以是ios::end（从流的末尾开始定位）
+     * 文件位置指针是个整数值，制定了从文件的其实位置到指针所在位置的字节数
+     */
+    // 定位到fileObject的第n各字节（假设是ios::beg）
+    //fileObject.seekg(n);
+
+    // 把文件的读指针从fileObject 当前位置后移n个字节
+    //fileObject.seekg(n, ios::cur);
+
+    // 把文件的读指针从fileObject 末尾往回移n个字节
+    //fileObject.seekg(n, ios::end);
+
+    // 定位到fileObject的末尾
+    //fileObject.seekg(0, ios::end);
 }
+
