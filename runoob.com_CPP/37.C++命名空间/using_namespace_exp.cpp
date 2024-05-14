d98bb2961905c5f8bfe4acb04852c51073259b33
JinLZhu <rains_lovelily_only@126.com>
2024-05-14 17:22:32 +0800

    Continue...

--- runoob.com_CPP/37.C++命名空间/using_namespace_exp.cpp
+++ runoob.com_CPP/37.C++命名空间/using_namespace_exp.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // 第一个命名空间
-namespace first_spare {
+namespace first_space {
     void func() {
         cout << "Inside first_spare" << endl;
     }
@@ -13,7 +13,7 @@ namespace second_spare {
         cout << "Inside second_spare" << endl;
     }
 }
-using namespace first_spare;
+using namespace first_space;
 int main()
 {
     // 调用第一个命名空间的函数
