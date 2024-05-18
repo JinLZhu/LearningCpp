54ceb784c5ef25d311da72b15b1efc56089d62dd
JinLZhu <rains_lovelily_only@126.com>
2024-05-18 21:03:53 +0800

    Continue...

---
+++ runoob.com_CPP/39.C++预处理器/predefined_micros.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Valus of __LINE__ : " << __LINE__ << endl;
+    cout << "Valus of __FILE__ : " << __FILE__ << endl;
+    cout << "Valus of __DATE__ : " << __DATE__ << endl;
+    cout << "Valus of __TIME__ : " << __TIME__ << endl;
+
+    return 0;
+}
